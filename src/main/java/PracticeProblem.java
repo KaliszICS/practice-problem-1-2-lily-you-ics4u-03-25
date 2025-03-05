e1fa4f78466c7604dc0934816bc5d7872be68072
lily <lily.you@student.tdsb.on.ca>
2025-03-05 03:10:09 +0000

    Completed 1.2

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,38 @@ public class PracticeProblem {
 
 	}
 
+	public static boolean validIndex(int[] arr, int num){
+		try{
+			for(int i = 0; i <= num; i++){
+				System.out.println(arr[i]);
+			}
+			return true;
+		}
+		catch(ArrayIndexOutOfBoundsException e){
+			return false;
+		}
+	}
 	
+	public static int divide(int num, int num2){
+		int result;
+		try{
+			result = num/num2;
+		}
+		catch(ArithmeticException e){
+			result = 0;
+		}
+		return result;
+	}
+
+	public static int safeConvertStringtoInt(String word){
+		int result;
+		try{
+			result = Integer.parseInt(word);
+		}
+		catch(NumberFormatException e){
+			result = 0;
+		}
+		return result;
+	}
 
 }
